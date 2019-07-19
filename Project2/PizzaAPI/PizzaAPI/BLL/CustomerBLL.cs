617437da9a634f0146ab775ad9b143a64f983d3d
rcheema97 <47155199+rcheema97@users.noreply.github.com>
2019-07-19 14:17:11 -0400

    Models

---
+++ Project2/PizzaAPI/PizzaAPI/BLL/CustomerBLL.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using PizzaAPI;
+using PizzaAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaAPI.BLL
+{
+    public class CustomerBLL:ICustomer
+    {
+        private PizzaDbContext _context;
+
+        public CustomerBLL(PizzaDbContext context)
+        {
+            _context = context;
+
+        }
+        public void AddCustomer(string JsonString)
+        {
+            Customer temp = JsonConvert.DeserializeObject<Customer>(JsonString);
+            _context.Customer.Add(temp);
+            _context.SaveChanges();
+        }
+
+        public List<String> ReturnAllCustomer()
+        {
+            List<String> allCustomers = new List<string>();
+            foreach (var item in _context.Customer)
+            {
+                var temp = JsonConvert.SerializeObject(item);
+                allCustomers.Add(temp);
+            }
+            return allCustomers;
+        }
+
+
+
+    }
+}
---
+++ Project2/PizzaAPI/PizzaAPI/Model/PizzaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaAPI.Model
+{
+    public class PizzaDbContext : DbContext
+    {
+        public PizzaDbContext(DbContextOptions<PizzaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Pizza> Pizza { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+
+    }
+}
